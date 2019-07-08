570e10493cb29ca3c0c1524ddc2cb6b0caf1a4a8
Jake Klassen <jklassendev@gmail.com>
2019-07-08 09:09:17 -0400

    release work

--- server/keys.js
+++ server/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT,
-  pgUser: process.env.PG_USER,
-  pgPassword: process.env.PG_PASSWORD,
-  pgHost: process.env.PG_HOST,
-  pgPort: process.env.PG_PORT,
-  pgDatabase: process.env.PG_DATABASE,
+  pgUser: process.env.PGUSER,
+  pgPassword: process.env.PGPASSWORD,
+  pgHost: process.env.PGHOST,
+  pgPort: process.env.PGPORT,
+  pgDatabase: process.env.PGDATABASE,
 };
